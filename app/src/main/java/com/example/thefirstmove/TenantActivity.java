d5395cda533fd1bb3a018c6f0fcdfdb13c9fc951
271047045@qq.com <qq123998!>
2020-06-24 20:46:49 +0800

    将主界面的图标更换

---
+++ app/src/main/java/com/example/thefirstmove/TenantActivity.java
@@ -0,0 +1,14 @@
+package com.example.thefirstmove;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TenantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant);
+    }
+}
